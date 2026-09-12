a1c57f29c0695f1bc76e96cad29ddd52710d64aa
Thomas Vincent <thomasvincent@gmail.com>
2026-09-12 10:09:48 -0700

    chore(i18n): use neutral catalog-script comments
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- locales/update-pot.sh
+++ locales/update-pot.sh
@@ -16,7 +16,7 @@
 # get script name
 SCRIPT_NAME=`basename ${0}`
 
-# locate base directory of Cacti
+# locate the application base directory
 REALPATH_BIN=`which realpath 2>/dev/null`
 if [ $? -gt 0 ]
 then
