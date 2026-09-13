e059a0ed8c7ffd99afe062e9f8389cf0dc8de2c6
Thomas Vincent <thomasvincent@gmail.com>
2026-09-13 12:57:48 -0700

    chore: convert core file headers to SPDX
    Inherited Cacti files keep their copyright years and take GPL-2.0-or-later.
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- lib/api_graph.php
+++ lib/api_graph.php
@@ -1,19 +1,8 @@
 <?php
 /*
- +-------------------------------------------------------------------------+
- | Copyright (C) 2004-2026 The Cacti Group                                 |
- |                                                                         |
- | This program is free software; you can redistribute it and/or           |
- | modify it under the terms of the GNU General Public License             |
- | as published by the Free Software Foundation; either version 2          |
- | of the License, or (at your option) any later version.                  |
- |                                                                         |
- | This program is distributed in the hope that it will be useful,         |
- | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
- | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
- | GNU General Public License for more details.                            |
- +-------------------------------------------------------------------------+
-*/
+ * SPDX-FileCopyrightText: 2004-2026 The Cacti Group
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 
 function api_delete_graphs(&$local_graph_ids, $delete_type) {
 	/* check for a bad local_graph_id = 0, and remove graphs */
--- lib/cdef.php
+++ lib/cdef.php
@@ -1,19 +1,8 @@
 <?php
 /*
- +-------------------------------------------------------------------------+
- | Copyright (C) 2004-2026 The Cacti Group                                 |
- |                                                                         |
- | This program is free software; you can redistribute it and/or           |
- | modify it under the terms of the GNU General Public License             |
- | as published by the Free Software Foundation; either version 2          |
- | of the License, or (at your option) any later version.                  |
- |                                                                         |
- | This program is distributed in the hope that it will be useful,         |
- | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
- | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
- | GNU General Public License for more details.                            |
- +-------------------------------------------------------------------------+
-*/
+ * SPDX-FileCopyrightText: 2004-2026 The Cacti Group
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 
 /* get_cdef_item_name - resolves a single CDEF item into its text-based representation
    @arg $cdef_item_id - the id of the individual cdef item
