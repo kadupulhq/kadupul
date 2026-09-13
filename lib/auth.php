b412c0f222a60a7a998dcf439181b4580ac65f51
Thomas Vincent <thomasvincent@gmail.com>
2026-09-12 22:54:09 -0700

    fix(auth): take the Web Basic user only from server-set variables
    HTTP_* variants are request headers and PHP fills PHP_AUTH_USER from the Authorization header, so neither shows that the web server authenticated the request. The identity is now taken only from REMOTE_USER, then REDIRECT_REMOTE_USER; a server that authenticates without exporting REMOTE_USER must pass it through.
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- lib/auth.php
+++ lib/auth.php
@@ -278,18 +278,13 @@ function get_basic_auth_username() {
 		return false;
 	}
 
-	if (isset($_SERVER['PHP_AUTH_USER'])) {
-		$username = str_replace("\\", "\\\\", $_SERVER['PHP_AUTH_USER']);
-	} elseif (isset($_SERVER['REMOTE_USER'])) {
+	/* HTTP_* entries are request headers, and PHP fills PHP_AUTH_USER from the
+	 * client's Authorization header whether or not the web server checked it.
+	 * Only REMOTE_USER is set by the server once it has authenticated the user. */
+	if (isset($_SERVER['REMOTE_USER'])) {
 		$username = str_replace("\\", "\\\\", $_SERVER['REMOTE_USER']);
 	} elseif (isset($_SERVER['REDIRECT_REMOTE_USER'])) {
 		$username = str_replace("\\", "\\\\", $_SERVER['REDIRECT_REMOTE_USER']);
-	} elseif (isset($_SERVER['HTTP_PHP_AUTH_USER'])) {
-		$username = str_replace("\\", "\\\\", $_SERVER['HTTP_PHP_AUTH_USER']);
-	} elseif (isset($_SERVER['HTTP_REMOTE_USER'])) {
-		$username = str_replace("\\", "\\\\", $_SERVER['HTTP_REMOTE_USER']);
-	} elseif (isset($_SERVER['HTTP_REDIRECT_REMOTE_USER'])) {
-		$username = str_replace("\\", "\\\\", $_SERVER['HTTP_REDIRECT_REMOTE_USER']);
 	} else {
 		$username = false;
 	}
