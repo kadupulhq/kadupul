5f72c0459e482300b9a4ad4d2c804b418f42f09d
Thomas Vincent <thomasvincent@gmail.com>
2026-09-13 18:10:10 -0700

    fix(poller): log a timeout kill only after it is delivered
    The sweep reported a kill before finding that a task started by another user could not be signalled.
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- lib/poller.php
+++ lib/poller.php
@@ -2992,12 +2992,13 @@ function timeout_kill_registered_processes($tasktype = '', $taskname = '', $task
 				cacti_log(sprintf('WARNING: Refusing to kill registered process with a reserved system PID! (%s, %s, %s, %s)', $r['tasktype'], $r['taskname'], $r['taskid'], $logged_pid), false, 'POLLER');
 
 			} elseif (cacti_process_still_running($pid)) {
-				cacti_log(sprintf('ERROR: Process killed due to timeout! (%s, %s, %s, %s)', $r['tasktype'], $r['taskname'], $r['taskid'], $logged_pid), false, 'POLLER');
 				if (!cacti_process_kill($pid, SIGTERM) && cacti_process_kill_denied($pid)) {
 					cacti_log(sprintf('ERROR: Process owned by another user could not be killed and stays registered! (%s, %s, %s, %s)', $r['tasktype'], $r['taskname'], $r['taskid'], $logged_pid), false, 'POLLER');
 
 					continue;
 				}
+
+				cacti_log(sprintf('ERROR: Process killed due to timeout! (%s, %s, %s, %s)', $r['tasktype'], $r['taskname'], $r['taskid'], $logged_pid), false, 'POLLER');
 			} else {
 				cacti_log(sprintf('ERROR: Detected process that is gone and did not unregister first! (%s, %s, %s, %s)', $r['tasktype'], $r['taskname'], $r['taskid'], $logged_pid), false, 'POLLER');
 			}
