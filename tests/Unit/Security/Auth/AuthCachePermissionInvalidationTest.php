33912c5a22bba82ee141c0f566dc46ae4f1ec9db
Thomas Vincent <thomasvincent@gmail.com>
2026-09-13 18:10:46 -0700

    chore: add the Kadupul copyright to files this branch changes
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- tests/Unit/Security/Auth/AuthCachePermissionInvalidationTest.php
+++ tests/Unit/Security/Auth/AuthCachePermissionInvalidationTest.php
@@ -2,6 +2,7 @@
 /*
  +-------------------------------------------------------------------------+
  | Copyright (C) 2004-2026 The Cacti Group                                 |
+ | Copyright (C) 2026 The Kadupul project and contributors                 |
  |                                                                         |
  | This program is free software; you can redistribute it and/or           |
  | modify it under the terms of the GNU General Public License             |
