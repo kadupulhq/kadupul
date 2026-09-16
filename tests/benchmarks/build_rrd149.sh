e10abb3100dce930f70e49ed7e0aaae3cb5bb166
Thomas Vincent <thomasvincent@gmail.com>
2026-09-15 20:16:10 -0700

    ci: bootstrap legacy libtool files explicitly
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- tests/benchmarks/build_rrd149.sh
+++ tests/benchmarks/build_rrd149.sh
@@ -9,6 +9,7 @@ curl --fail --location --retry 3 https://codeload.github.com/oetiker/rrdtool-1.x
 printf '%s  %s\n' c125d6850c7b6a24a18676989c617e82f0e594444017c833b92da8ffa759d270 "$source_dir/source.tar.gz" | sha256sum --check
 tar xzf "$source_dir/source.tar.gz" -C "$source_dir" --strip-components=1
 cd "$source_dir"
+libtoolize --force --copy
 autoreconf -fi
 ./configure --prefix="$prefix" --disable-perl --disable-python --disable-ruby --disable-tcl --disable-lua --disable-libdbi --disable-rrdcached
 make -j2
