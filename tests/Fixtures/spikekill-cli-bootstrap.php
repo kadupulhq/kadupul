651d823d1bbad4cac7dc50c46a8a7bfacf1ab2a0
Thomas Vincent <thomasvincent@gmail.com>
2026-09-15 18:32:44 -0700

    fix(spikekill): handle sparse statistics and bound process waits
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- tests/Fixtures/spikekill-cli-bootstrap.php
+++ tests/Fixtures/spikekill-cli-bootstrap.php
@@ -3,6 +3,14 @@
 // SPDX-FileCopyrightText: 2026 The Kadupul project and contributors
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+error_reporting(E_ALL);
+set_error_handler(function ($severity, $message, $file, $line) {
+    if (error_reporting() & $severity) {
+        throw new ErrorException($message, 0, $severity, $file, $line);
+    }
+    return false;
+});
+
 $config = array('base_path' => getenv('SPIKE_TEST_ROOT'), 'poller_id' => 1, 'cacti_server_os' => 'unix');
 function read_config_option($name, $default = '')
 {
