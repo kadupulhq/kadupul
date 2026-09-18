9c76b2fc23c0eea372a704f880fe0fb63b454567
Thomas Vincent <thomasvincent@gmail.com>
2026-09-18 10:14:15 -0700

    style: convert 4 files to PER-CS 2.0
    lib/poller.php
    lib/rrd.php
    include/global_settings.php
    install/upgrades/1_2_31.php
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- install/upgrades/1_2_31.php
+++ install/upgrades/1_2_31.php
@@ -1,26 +1,27 @@
 <?php
+
 /*
  * SPDX-FileCopyrightText: 2004-2026 The Cacti Group
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-function upgrade_to_1_2_31() {
-	global $config;
+function upgrade_to_1_2_31()
+{
+    global $config;
 
-	db_install_execute('ALTER TABLE automation_devices MODIFY COLUMN snmp_priv_protocol char(7) default ""');
-	db_install_execute('ALTER TABLE automation_snmp_items MODIFY COLUMN snmp_priv_protocol char(7) default ""');
-	db_install_execute('ALTER TABLE snmpagent_managers MODIFY COLUMN snmp_priv_protocol char(7) NOT NULL');
+    db_install_execute('ALTER TABLE automation_devices MODIFY COLUMN snmp_priv_protocol char(7) default ""');
+    db_install_execute('ALTER TABLE automation_snmp_items MODIFY COLUMN snmp_priv_protocol char(7) default ""');
+    db_install_execute('ALTER TABLE snmpagent_managers MODIFY COLUMN snmp_priv_protocol char(7) NOT NULL');
 
-	db_install_execute('ALTER TABLE settings MODIFY COLUMN `name` varchar(255) NOT NULL default ""');
+    db_install_execute('ALTER TABLE settings MODIFY COLUMN `name` varchar(255) NOT NULL default ""');
 
-	if (!db_index_exists('snmp_query_graph', 'snmp_query_id')) {
-		db_install_execute('ALTER TABLE snmp_query_graph ADD INDEX snmp_query_id (snmp_query_id)');
-	}
+    if (!db_index_exists('snmp_query_graph', 'snmp_query_id')) {
+        db_install_execute('ALTER TABLE snmp_query_graph ADD INDEX snmp_query_id (snmp_query_id)');
+    }
 
-	if (!db_index_exists('snmp_query_graph', 'graph_template_id')) {
-		db_install_execute('ALTER TABLE snmp_query_graph ADD INDEX graph_template_id (graph_template_id)');
-	}
+    if (!db_index_exists('snmp_query_graph', 'graph_template_id')) {
+        db_install_execute('ALTER TABLE snmp_query_graph ADD INDEX graph_template_id (graph_template_id)');
+    }
 
-	db_install_execute('ALTER TABLE settings_user MODIFY COLUMN name varchar(255) NOT NULL default ""');
+    db_install_execute('ALTER TABLE settings_user MODIFY COLUMN name varchar(255) NOT NULL default ""');
 }
-
