2e8152244fe18196d97c18030083266b7e90c4c7
Thomas Vincent <thomasvincent@gmail.com>
2026-09-17 18:32:42 -0700

    test(maintenance): isolate subprocess temporary directories
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- tests/Unit/Core/Cli/RrdMaintenanceCoordinationTest.php
+++ tests/Unit/Core/Cli/RrdMaintenanceCoordinationTest.php
@@ -137,7 +137,7 @@ FIXTURE;
             }
             $lock = $busy ? rrd_maintenance_acquire() : null;
         }
-        $process = proc_open($args, array(0 => array('pipe', 'r'), 1 => array('pipe', 'w'), 2 => array('pipe', 'w')), $pipes, null, array_merge(getenv(), array('RRDCACHED_ADDRESS' => $cached ? 'unix:/unavailable-test-cache' : '')));
+        $process = proc_open($args, array(0 => array('pipe', 'r'), 1 => array('pipe', 'w'), 2 => array('pipe', 'w')), $pipes, null, array_merge(getenv(), array('TMPDIR' => $dir, 'TMP' => $dir, 'TEMP' => $dir, 'RRDCACHED_ADDRESS' => $cached ? 'unix:/unavailable-test-cache' : '')));
         $deadline = microtime(true) + 10;
         while (!file_exists($dir . '/started') && microtime(true) < $deadline) {
             usleep(10000);
