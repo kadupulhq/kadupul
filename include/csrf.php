43045ba6627041295f92325e32fa7d90594b1271
Thomas Vincent <thomasvincent@gmail.com>
2026-09-18 17:15:34 -0700

    style: convert include/ and cli/ files the hardening series edits to PER-CS 2.0
    include/csrf.php
    include/global.php
    cli/rebuild_poller_cache.php
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- include/csrf.php
+++ include/csrf.php
@@ -1,38 +1,41 @@
 <?php
+
 /*
  * SPDX-FileCopyrightText: 2004-2026 The Cacti Group
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-require_once($config['include_path'] .'/vendor/csrf/csrf-conf.php');
+require_once($config['include_path'] . '/vendor/csrf/csrf-conf.php');
 
 /* cross site request forgery library */
-function csrf_startup() {
-	global $config;
+function csrf_startup()
+{
+    global $config;
 
-	if ($config['is_web']) {
-		/* If you need to debug CSRF, uncomment the following line */
-		//csrf_conf('log_file', dirname(read_config_option('path_cactilog')) . '/csrf.log');
-		if (!empty($config['path_csrf_secret'])) {
-			csrf_conf('path_secret', $config['path_csrf_secret']);
-		}
+    if ($config['is_web']) {
+        /* If you need to debug CSRF, uncomment the following line */
+        //csrf_conf('log_file', dirname(read_config_option('path_cactilog')) . '/csrf.log');
+        if (!empty($config['path_csrf_secret'])) {
+            csrf_conf('path_secret', $config['path_csrf_secret']);
+        }
 
-		csrf_conf('rewrite-js', $config['url_path'] . 'include/vendor/csrf/csrf-magic.js');
-		csrf_conf('callback', 'csrf_error_callback');
-		csrf_conf('expires', 7200);
-	} else {
-		csrf_conf('disable',true);
-	}
+        csrf_conf('rewrite-js', $config['url_path'] . 'include/vendor/csrf/csrf-magic.js');
+        csrf_conf('callback', 'csrf_error_callback');
+        csrf_conf('expires', 7200);
+    } else {
+        csrf_conf('disable', true);
+    }
 }
 
-function csrf_error_callback() {
-	//Resolve session fixation for PHP 5.4
-	session_regenerate_id();
-	raise_message('csrf_timeout');
-	ob_end_clean();
-	header('Location: ' . validate_redirect_url($_SERVER['REQUEST_URI']));
-	csrf_log(__FUNCTION__, 'Timeout, redirecting to ' . validate_redirect_url($_SERVER['REQUEST_URI']));
-	exit;
+function csrf_error_callback()
+{
+    //Resolve session fixation for PHP 5.4
+    session_regenerate_id();
+    raise_message('csrf_timeout');
+    ob_end_clean();
+    header('Location: ' . validate_redirect_url($_SERVER['REQUEST_URI']));
+    csrf_log(__FUNCTION__, 'Timeout, redirecting to ' . validate_redirect_url($_SERVER['REQUEST_URI']));
+    exit;
 }
 
 include_once($config['include_path'] . '/vendor/csrf/csrf-magic.php');
