ff8864c92124ebfdbf6636af6c5cd8ee95028587
Thomas Vincent <thomasvincent@gmail.com>
2026-09-21 01:18:29 -0700

    fix(security): escape LTS graph and site search attributes
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- links.php
+++ links.php
@@ -326,10 +326,14 @@ function pages() {
 			<table class='filterTable' cellpadding='2' cellspacing='0'>
 				<tr>
 					<td>
-						<?php print __('Search');?>
+						<label for='filter'><?php print __('Search');?></label>
 					</td>
 					<td>
-						<input type='text' class='ui-state-default ui-corner-all' id='filter' size='25' value='<?php print html_escape_request_var('filter');?>'>
+						<input type='text' class='ui-state-default ui-corner-all' id='filter' size='25' value='<?php
+							$search_html = (string) get_request_var('filter');
+							$search_html = htmlspecialchars($search_html, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+							print str_replace('`', '&#96;', $search_html);
+						?>'>
 					</td>
 					<td>
 						<?php print __('Links');?>
