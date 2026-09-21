8258863445d86612b3f6f3cd1cb4d6c94cc2de8c
Thomas Vincent <thomasvincent@gmail.com>
2026-09-21 01:10:11 -0700

    fix(security): escape LTS data template search attributes
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- cdef.php
+++ cdef.php
@@ -750,10 +750,14 @@ function cdef() {
 			<table class='filterTable'>
 				<tr>
 					<td>
-						<?php print __('Search');?>
+						<label for='filter'><?php print __('Search');?></label>
 					</td>
 					<td>
-						<input type='text' class='ui-state-default ui-corner-all' id='filter' name='filter' size='25' value='<?php print html_escape_request_var('filter');?>'>
+						<input type='text' class='ui-state-default ui-corner-all' id='filter' name='filter' size='25' value='<?php
+							$search_html = (string) get_request_var('filter');
+							$search_html = htmlspecialchars($search_html, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+							print str_replace('`', '&#96;', $search_html);
+						?>'>
 					</td>
 					<td>
 						<?php print __('CDEFs');?>
