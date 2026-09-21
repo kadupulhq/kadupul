34972b80be36e43719e996dfe2324fcc0d85d738
Thomas Vincent <thomasvincent@gmail.com>
2026-09-20 19:02:16 -0700

    style(lts): simplify whitelist message severity expression
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- data_input.php
+++ data_input.php
@@ -91,11 +91,8 @@ switch (get_request_var('action')) {
 			'--update', '--push', '--id=' . (int) $id
 		), $output, null);
 
-		raise_message(
-			'whitelist_updated',
-			html_escape(implode("\n", $output)),
-			$status === 0 ? MESSAGE_LEVEL_INFO : MESSAGE_LEVEL_ERROR
-		);
+		$level = $status === 0 ? MESSAGE_LEVEL_INFO : MESSAGE_LEVEL_ERROR;
+		raise_message('whitelist_updated', html_escape(implode("\n", $output)), $level);
 
 		/* fall through */
 	case 'edit':
