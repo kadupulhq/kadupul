a9cd6a88962370329e42e72a631bbedd98e398e3
Thomas Vincent <thomasvincent@gmail.com>
2026-09-21 00:08:11 -0700

    style(lts): align new VDEF escaping statements
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- vdef.php
+++ vdef.php
@@ -294,11 +294,11 @@ function vdef_form_actions() {
 		exit;
 	}
 
-    $selected_items_html = (isset($vdef_array) ? serialize($vdef_array) : '');
-    $selected_items_html = htmlspecialchars($selected_items_html, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
-    $selected_items_html = str_replace('`', '&#96;', $selected_items_html);
-    $action_html = htmlspecialchars((string) get_nfilter_request_var('drp_action'), ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
-    $action_html = str_replace('`', '&#96;', $action_html);
+	$selected_items_html = (isset($vdef_array) ? serialize($vdef_array) : '');
+	$selected_items_html = htmlspecialchars($selected_items_html, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+	$selected_items_html = str_replace('`', '&#96;', $selected_items_html);
+	$action_html = htmlspecialchars((string) get_nfilter_request_var('drp_action'), ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+	$action_html = str_replace('`', '&#96;', $action_html);
 
     print "<tr>
         <td class='saveRow'>
