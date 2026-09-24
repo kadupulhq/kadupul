7ab1adbb2f47a6406a796d32aae9ef647b14f1ab
Thomas Vincent <thomasvincent@gmail.com>
2026-09-24 16:57:00 -0700

    fix(rrd): send DEF paths to the RRDtool proxy bare and relative
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- src/Graphing/Infrastructure/Rrd/UnrepresentableArgument.php
+++ src/Graphing/Infrastructure/Rrd/UnrepresentableArgument.php
@@ -9,9 +9,9 @@ namespace Kadupul\Graphing\Infrastructure\Rrd;
 
 final class UnrepresentableArgument extends \InvalidArgumentException
 {
-    public function __construct()
+    // The argument itself is left out: it may be a device value or a path.
+    public function __construct(string $message = 'An RRDtool pipe argument cannot contain NUL, CR or LF.')
     {
-        // The argument itself is left out: it may be a device value or a path.
-        parent::__construct('An RRDtool pipe argument cannot contain NUL, CR or LF.');
+        parent::__construct($message);
     }
 }
