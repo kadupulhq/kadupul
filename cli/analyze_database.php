012317b2c53ee9b9b8cdf6a32a004054781c2abb
Thomas Vincent <thomasvincent@gmail.com>
2026-09-24 02:23:48 -0700

    style: convert cli/analyze_database.php to PER-CS 2.0
    Formatting only, so the shim edit that follows shows only behaviour changes.
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- cli/analyze_database.php
+++ cli/analyze_database.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
+
 /*
  * SPDX-FileCopyrightText: 2004-2026 The Cacti Group
  * SPDX-FileCopyrightText: 2026 The Kadupul project and contributors
@@ -20,82 +21,84 @@ $form  = '';
 $start = time();
 
 if (cacti_sizeof($parms)) {
-	foreach($parms as $parameter) {
-		if (strpos($parameter, '=')) {
-			list($arg, $value) = explode('=', $parameter, 2);
-		} else {
-			$arg = $parameter;
-			$value = '';
-		}
+    foreach ($parms as $parameter) {
+        if (strpos($parameter, '=')) {
+            list($arg, $value) = explode('=', $parameter, 2);
+        } else {
+            $arg = $parameter;
+            $value = '';
+        }
 
-		switch ($arg) {
-			case '-d':
-			case '--debug':
-				$debug = true;
-				break;
-			case '--local':
-				$local = true;
-				break;
-			case '--version':
-			case '-V':
-			case '-v':
-				display_version();
-				exit(0);
-			case '--help':
-			case '-H':
-			case '-h':
-				display_help();
-				exit(0);
-			default:
-				print "ERROR: Invalid Parameter " . $parameter . "\n\n";
-				display_help();
-				exit(1);
-		}
-	}
+        switch ($arg) {
+            case '-d':
+            case '--debug':
+                $debug = true;
+                break;
+            case '--local':
+                $local = true;
+                break;
+            case '--version':
+            case '-V':
+            case '-v':
+                display_version();
+                exit(0);
+            case '--help':
+            case '-H':
+            case '-h':
+                display_help();
+                exit(0);
+            default:
+                print "ERROR: Invalid Parameter " . $parameter . "\n\n";
+                display_help();
+                exit(1);
+        }
+    }
 }
 
 print "NOTE: Analyzing All Kadupul Database Tables\n";
 
 if (!$local && $config['poller_id'] > 1) {
-	db_switch_remote_to_main();
+    db_switch_remote_to_main();
 
-	print "NOTE: Repairing Tables for Main Database" . PHP_EOL;
+    print "NOTE: Repairing Tables for Main Database" . PHP_EOL;
 } else {
-	print "NOTE: Repairing Tables for Local Database" . PHP_EOL;
+    print "NOTE: Repairing Tables for Local Database" . PHP_EOL;
 }
 
 $tables = db_fetch_assoc('SHOW TABLES FROM `' . $database_default . '`');
 
 if (cacti_sizeof($tables)) {
-	foreach($tables AS $table) {
-		if (db_binlog_enabled()) {
-			print "NOTE: Analyzing Table -> '" . $table['Tables_in_' . $database_default] . "' without writing to the binlog";
-			$status = db_execute('ANALYZE TABLE NO_WRITE_TO_BINLOG ' . $table['Tables_in_' . $database_default] . $form);
-		} else {
-			print "NOTE: Analyzing Table -> '" . $table['Tables_in_' . $database_default] . "'";
-			$status = db_execute('ANALYZE TABLE ' . $table['Tables_in_' . $database_default] . $form);
-		}
+    foreach ($tables AS $table) {
+        if (db_binlog_enabled()) {
+            print "NOTE: Analyzing Table -> '" . $table['Tables_in_' . $database_default] . "' without writing to the binlog";
+            $status = db_execute('ANALYZE TABLE NO_WRITE_TO_BINLOG ' . $table['Tables_in_' . $database_default] . $form);
+        } else {
+            print "NOTE: Analyzing Table -> '" . $table['Tables_in_' . $database_default] . "'";
+            $status = db_execute('ANALYZE TABLE ' . $table['Tables_in_' . $database_default] . $form);
+        }
 
-		print ($status == 0 ? ' Failed' : ' Successful') . "\n";
-	}
+        print ($status == 0 ? ' Failed' : ' Successful') . "\n";
+    }
 
-	cacti_log('ANALYSIS STATS: Analyzing Kadupul Tables Complete.  Total time ' . (time() - $start) . ' seconds.', false, 'SYSTEM');
+    cacti_log('ANALYSIS STATS: Analyzing Kadupul Tables Complete.  Total time ' . (time() - $start) . ' seconds.', false, 'SYSTEM');
 }
 
 /*  display_version - displays version information */
-function display_version() {
-	$version = get_cacti_cli_version();
-	print "Kadupul Analyze Database Utility, Version $version, " . COPYRIGHT_YEARS . "\n";
+function display_version()
+{
+    $version = get_cacti_cli_version();
+    print "Kadupul Analyze Database Utility, Version $version, " . COPYRIGHT_YEARS . "\n";
 }
 
 /*	display_help - displays the usage of the function */
-function display_help () {
-	display_version();
+function display_help()
+{
+    display_version();
 
-	print "\nusage: analyze_database.php [-d|--debug]\n\n";
-	print "A utility to recalculate the cardinality of indexes within the Kadupul database.\n";
-	print "It's important to periodically run this utility especially on larger systems.\n\n";
-	print "Optional:\n";
-	print "     --local   - Perform the action on the Remote Data Collector if run from there\n";
-	print "-d | --debug   - Display verbose output during execution\n\n";
+    print "\nusage: analyze_database.php [-d|--debug]\n\n";
+    print "A utility to recalculate the cardinality of indexes within the Kadupul database.\n";
+    print "It's important to periodically run this utility especially on larger systems.\n\n";
+    print "Optional:\n";
+    print "     --local   - Perform the action on the Remote Data Collector if run from there\n";
+    print "-d | --debug   - Display verbose output during execution\n\n";
 }
