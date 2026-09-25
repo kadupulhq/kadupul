4904ff353a40d5e7f5b29af00a71f79e8d1a5f39
Thomas Vincent <thomasvincent@users.noreply.github.com>
2026-09-25 15:29:11 -0700

    fix(security): harden LTS theme and graph ordering
    Signed-off-by: Thomas Vincent <thomasvincent@users.noreply.github.com>

--- graph_templates_items.php
+++ graph_templates_items.php
@@ -397,9 +397,7 @@ function item_movedown() {
 		move_graph_group(get_request_var('id'), $arr, $next_id, 'next');
 	} elseif (!preg_match('/(AREA|STACK|LINE)/', $text_type)) {
 		/* this is so we know the "other" graph item to propagate the changes to */
-		$next_item = get_item('graph_templates_item', 'sequence', get_request_var('id'), array('graph_template_id' => get_request_var('graph_template_id'), 'local_graph_id' => 0), 'next');
-
-		move_item_down('graph_templates_item', get_request_var('id'), 'graph_template_id=' . get_request_var('graph_template_id') . ' AND local_graph_id=0');
+		move_item_down('graph_templates_item', get_request_var('id'), array('graph_template_id' => get_request_var('graph_template_id'), 'local_graph_id' => 0));
 	}
 
 	if (!isempty_request_var('graph_template_id')) {
@@ -429,9 +427,7 @@ function item_moveup() {
 		move_graph_group(get_request_var('id'), $arr, $next_id, 'previous');
 	} elseif (!preg_match('/(AREA|STACK|LINE)/', $text_type)) {
 		/* this is so we know the "other" graph item to propagate the changes to */
-		$last_item = get_item('graph_templates_item', 'sequence', get_request_var('id'), array('graph_template_id' => get_request_var('graph_template_id'), 'local_graph_id' => 0), 'previous');
-
-		move_item_up('graph_templates_item', get_request_var('id'), 'graph_template_id=' . get_request_var('graph_template_id') . ' AND local_graph_id=0');
+		move_item_up('graph_templates_item', get_request_var('id'), array('graph_template_id' => get_request_var('graph_template_id'), 'local_graph_id' => 0));
 	}
 
 	if (!isempty_request_var('graph_template_id')) {
