1ba47972472e937dc8a6b95ded1f7084bebd3098
Thomas Vincent <thomasvincent@gmail.com>
2026-09-26 03:35:02 -0700

    fix(tests): restore PHPUnit coverage access under Pest 4
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,4 +4,22 @@
  * Pest bootstrap.
  */
 
-uses(PHPUnit\Framework\TestCase::class)->in('Unit', 'integration', 'mutation', 'handoff');
+use PHPUnit\Runner\CodeCoverage;
+use SebastianBergmann\CodeCoverage\CodeCoverage as CoverageReport;
+
+trait PestCodeCoverageCompatibility
+{
+    public function getTestResultObject(): object
+    {
+        return new class {
+            public function getCodeCoverage(): ?CoverageReport
+            {
+                $runner = CodeCoverage::instance();
+
+                return $runner->isActive() ? $runner->codeCoverage() : null;
+            }
+        };
+    }
+}
+
+uses(PHPUnit\Framework\TestCase::class, PestCodeCoverageCompatibility::class)->in('Unit', 'integration', 'mutation', 'handoff');
